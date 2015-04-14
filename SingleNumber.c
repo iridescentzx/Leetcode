819d2b54fb54c9e307d45f89712b412886b6b515
iridescentzx <xz2410@columbia.edu>
2015-04-14 13:47:17 -0500

    Create SingleNumber.c

---
+++ SingleNumber.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int singleNumber(int A[], int n) {
+    int sum, i;
+    sum = A[0];
+    for(i = 1; i < n; i++){
+        sum = A[i] ^ sum;
+    }
+    return sum;
+}
+int main(){
+	int n;
+	scanf("%d", &n);
+	int i, A[n];
+	for(i = 0;i<n;i++){
+		scanf("%d", &A[i]);
+	}
+	int s;
+	s = singleNumber(A, n);
+	printf("%d\n", s);
+	return 0;
+
+} 
